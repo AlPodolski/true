06930a7e4920002119dd4b459203e2a43719d5a5
sanya <sanyapodolski@gmail.com>
2020-12-23 09:36:12 +0200

    убрана карта с главной

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -130,45 +130,4 @@ Yii::$app->view->registerMetaTag([
             </p>
         </div>
     </div>
-
-    <article class="post map mobile-map">
-        <div class="post-img position-relative">
-            <img src="/img/map.png" alt="">
-            <div class="post-rating">
-                <div class="geo-bg">
-                </div>
-            </div>
-            <div class="pin-wrap">
-                <div class="pin" style="top: 250px;left: 100px;">
-                    <img src="/img/pin.png" alt="">
-                </div>
-                <div class="pin" style="top: 367px;left: 100px;">
-                    <img src="/img/pin.png" alt="">
-                </div>
-                <div class="pin" style="top: 357px;left: 130px;">
-                    <img src="/img/pin.png" alt="">
-                </div>
-                <div class="pin" style="top: 315px;left: 130px;">
-                    <img src="/img/pin.png" alt="">
-                </div>
-            </div>
-            <div class="user-on-map position-absolute d-flex">
-                <div class="user-map-img">
-                    <img src="/img/small-girl-map.png" alt="">
-                </div>
-                <div class="user-map-info">
-                    <div class="name">Лариса</div>
-                    <div class="post-address">
-                        <div class="geo-icon icon">
-                            <svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M4.50001 0C2.84583 0 1.5 1.34583 1.5 3.00001C1.5 3.49659 1.62415 3.98895 1.86018 4.42566L4.33595 8.90332C4.36891 8.96302 4.43172 9 4.50001 9C4.5683 9 4.6311 8.96302 4.66406 8.90332L7.14075 4.42419C7.37586 3.98895 7.50001 3.49657 7.50001 2.99999C7.50001 1.34583 6.15418 0 4.50001 0ZM4.50001 4.5C3.67292 4.5 3.00001 3.82709 3.00001 3.00001C3.00001 2.17292 3.67292 1.50001 4.50001 1.50001C5.32709 1.50001 6 2.17292 6 3.00001C6 3.82709 5.32709 4.5 4.50001 4.5Z" fill="white"/>
-                            </svg>
-                        </div>м. Авиамоторная</div>
-                </div>
-            </div>
-        </div>
-        <div class="red-btn-wrap">
-            <a class="red-btn" href="#">Поиск<img src="/img/up-arrow1.png" alt=""></a>
-        </div>
-    </article>
 </div>
