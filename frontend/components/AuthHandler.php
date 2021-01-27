10070a8310d4a91bfd0745db2e930070cd8c5a27
sanya <sanyapodolski@gmail.com>
2021-01-27 13:20:18 +0200

    добавление ид города к таблице пользователей

--- frontend/components/AuthHandler.php
+++ frontend/components/AuthHandler.php
@@ -106,7 +106,7 @@ class AuthHandler
             'created_at' => $time = time(),
             'updated_at' => $time,
             'status' => 10,
-            'city_ud' => $city,
+            'city_id' => $city,
         ]);
     }
 
