d67009ae267a69a1cc58e66065b6971f3e595994
sanya <sanyapodolski@gmail.com>
2021-05-19 16:35:29 +0300

    каноника на пагинации

---
+++ frontend/components/helpers/CanonicalHelper.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace frontend\components\helpers;
+
+
+class CanonicalHelper
+{
+    public static function getLink($request)
+    {
+
+        if (\strpos($request, 'page-') !== false) {
+
+            $link = \strstr($request, 'page', true);
+
+            if ($link == '/') return $link;
+
+            else return \mb_substr($link, 0, -1);
+
+        }
+
+        return false;
+
+    }
+}
--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -40,6 +40,9 @@ AppAsset::register($this);
     <link rel="icon" type="image/png" sizes="32x32" href="/img/favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="96x96" href="/img/favicons/favicon-96x96.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/img/favicons/favicon-16x16.png">
+    <?php if ($link = \frontend\components\helpers\CanonicalHelper::getLink(Yii::$app->request->url)) : ?>
+        <link rel="canonical" href="https://<?php echo Yii::$app->request->serverName . $link ?>">
+    <?php endif; ?>
     <link rel="manifest" href="/img/favicons/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="/img/favicons/ms-icon-144x144.png">
