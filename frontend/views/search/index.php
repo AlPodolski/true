cd3af35d61b87dd6b83d7bee272ad0d5a095a67d
sanya <sanyapodolski@gmail.com>
2021-05-21 13:32:13 +0300

    Правки со страницей поиска по имени

--- frontend/views/search/index.php
+++ frontend/views/search/index.php
@@ -11,11 +11,19 @@
     <h1 class="margin-top-20"> Поиск по имени : <?php echo $name ?></h1>
     <div class="row">
 
-        <?php foreach ($prPosts as $post) : ?>
+        <?php if ($prPosts) : foreach ($prPosts as $post) : ?>
 
             <?php echo $this->renderFile(Yii::getAlias('@app/views/layouts/article.php'), ['post' => $post]); ?>
 
         <?php endforeach; ?>
+
+        <?php else : ?>
+
+        <div class="col-12">
+            <p class="alert alert-info">По вашему запросу ничего не найдено попробуйте поискать другое имя</p>
+        </div>
+
+        <?php endif; ?>
 
     </div>
 </div>
