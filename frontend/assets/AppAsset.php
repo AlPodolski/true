848939a59a60a8974d8132a7a70d869e4a53a40f
sanya <sanyapodolski@gmail.com>
2021-06-16 10:13:39 +0300

    оработка окна диалогов

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,10 +12,10 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css?v=13',
+        'css/site.css?v=14',
     ];
     public $js = [
-        'js/script.js?v=6'
+        'js/script.js?v=7'
     ];
     public $depends = [
         'yii\web\YiiAsset',
