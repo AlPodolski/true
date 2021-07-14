bd481de1dbf27aba955cf02f689dcd1829835129
sanya <sanyapodolski@gmail.com>
2021-07-14 11:19:55 +0300

    отправка номера в админку при заказе звонка

--- frontend/models/forms/GetCallForm.php
+++ frontend/models/forms/GetCallForm.php
@@ -3,7 +3,10 @@
 
 namespace frontend\models\forms;
 
+use common\models\City;
 use common\models\RequestCall;
+use frontend\modules\user\models\Posts;
+use Yii;
 use yii\base\Model;
 
 class GetCallForm extends Model
@@ -38,6 +41,19 @@ class GetCallForm extends Model
         $requestCall->phone = $this->phone;
         $requestCall->user_id = $this->user_id;
         $requestCall->post_id = $this->post_id;
+
+        $post = Posts::find()->where(['id' => $this->post_id])->limit(1)->one();
+
+        $city = City::find()->where(['id' => $post['city_id']])->one();
+
+        $result = \json_decode(Yii::$app->phone->send(
+            [
+                'phone' => $this->phone,
+                'action' => 'add-phone',
+                'city' => $city['city'],
+            ]
+        ));
+
 
         return $requestCall->save();
     }
