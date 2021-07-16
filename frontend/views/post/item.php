b7595f001b1c6d6917376f545b7072590bcf1200
sanya <sanyapodolski@gmail.com>
2021-07-16 13:39:11 +0300

    правки c размером фото на деталке

--- frontend/views/post/item.php
+++ frontend/views/post/item.php
@@ -62,7 +62,7 @@ $countReview = \frontend\modules\user\models\Posts::countReview($post['id']);
                             <span class="active carousel-item">
                                 <?php echo PhotoWidget::widget([
                                     'path' => $post['avatar']['file'],
-                                    'size' => '360_485',
+                                    'size' => '360_471',
                                     'width' => true,
                                     'options' => [
                                         'class' => 'img user-img card_img',
@@ -83,7 +83,7 @@ $countReview = \frontend\modules\user\models\Posts::countReview($post['id']);
 
                                         <?php echo PhotoWidget::widget([
                                             'path' => $item['file'],
-                                            'size' => '360_485',
+                                            'size' => '360_471',
                                             'width' => true,
                                             'options' => [
                                                 'class' => 'img user-img',
