c51f8ad446749b98e77a6e1cefc4fb4bdc0a13dc
sanya <sanyapodolski@gmail.com>
2021-07-30 13:58:54 +0300

    крон для поднятий анкет ланы

---
+++ console/controllers/CronController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace console\controllers;
+
+use frontend\modules\user\models\Posts;
+use frontend\modules\user\models\TopAnketBlock;
+use yii\console\Controller;
+
+class CronController extends Controller
+{
+    public function actionUp ()
+    {
+        $posts = Posts::find()
+            ->with('avatar')
+            ->where(['user_id' => 22038])
+            ->andWhere(['status' => Posts::POST_ON_PUPLICATION_STATUS])
+            ->limit(3)
+            ->orderBy('RAND()')
+            ->one();
+
+        foreach ($posts as $post){
+
+            $upAnketModel = new TopAnketBlock();
+
+            $upAnketModel->post_id = $post['id'];
+            $upAnketModel->city_id = 1;
+            $upAnketModel->valid_to = \time() + 3600;
+
+            $upAnketModel->save();
+
+        }
+    }
+}
