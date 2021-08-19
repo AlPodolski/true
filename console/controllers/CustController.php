b4b057f6c288bb866f938a8936ed872d7b68a72e
sanya <sanyapodolski@gmail.com>
2021-08-19 12:37:43 +0300

    понижение цен

--- console/controllers/CustController.php
+++ console/controllers/CustController.php
@@ -14,28 +14,17 @@ class CustController extends Controller
 {
     public function actionIndex()
     {
-        $posts = Posts::findAll(['user_id' => null]);
+        $posts = Posts::findAll(['fake' => 0]);
 
         foreach ($posts as $post){
 
-            if (PostSites::find()->where(['post_id' => $post->id])->count() == 1 ){
+            if ($post->price > 1000 and $post->price < 2000) $post->price = $post->price - 500;
+            elseif ($post->price >= 2000 and $post->price < 4000) $post->price = $post->price - 1000;
+            elseif ($post->price >= 4000 and $post->price < 6000) $post->price = $post->price - 1500;
+            elseif ($post->price >= 6000 and $post->price < 8000) $post->price = $post->price - 2000;
+            elseif ($post->price > 8000) $post->price = $post->price - 3000;
 
-                $user = new User();
-                $user->username = $post->name;
-                $user->email = 'admin@mail.com';
-                $user->city_id = $post['city_id'];
-                $user->setPassword(Yii::$app->security->generateRandomString(10));
-                $user->generateAuthKey();
-                $user->generateEmailVerificationToken();
-                if ($user->save()) {
-
-                    $post->user_id = $user->id;
-
-                    $post->save();
-
-                }
-
-            }
+            $post->save();
 
         }
 
