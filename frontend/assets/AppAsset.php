c579efed58f3ed1faa2ad5915803d9857bb496b0
sanya <sanyapodolski@gmail.com>
2021-12-27 09:15:23 +0200

    правки

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,10 +12,10 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css?v=30',
+        'css/site.css?v=31',
     ];
     public $js = [
-        'js/script.js?v=22'
+        'js/script.js?v=23'
     ];
     public $depends = [
         'yii\web\YiiAsset',
