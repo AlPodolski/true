b46cbc001a60d2da8750588c217f5c069e4364e0
sanya <sanyapodolski@gmail.com>
2022-02-17 12:48:08 +0200

    страница с номерами телефонов адаптив

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,7 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css?v=51',
+        'css/site.css?v=52',
     ];
     public $js = [
         'js/script.js?v=31'
