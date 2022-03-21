eaf8de445259fe1c199f31b5b6c3a3f16be09b96
sanya <sanyapodolski@gmail.com>
2022-03-21 08:10:14 +0200

    правки

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -4,7 +4,7 @@ return [
     'bsVersion' => '4.x',
     'advert_limit' => 20,
     'post_limit' => 12,
-    'site_name' => 'sex-true.com',
+    'site_name' => 'sex-tut.com',
     'min_pay' => 300,
     'view_100_buy_price' => 20,
     'up_anket_cost' => 10,
