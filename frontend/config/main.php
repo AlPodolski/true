b1dcaea9386d4fb7f86f326f138e308639256ef5
Your Name <you@example.com>
2022-05-03 14:03:07 +0300

    правки с отправкой в теоегу

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -24,37 +24,6 @@ return [
         ],
     ],
     'components' => [
-        'imageCache' => [
-            'class' => 'frontend\components\service\image\ImageCache',
-            'sourcePath' => '@app/web/uploads',
-            'sourceUrl' => '@web/uploads',
-            'thumbsPath' => '@app/web/thumbs',
-            'extensions' => [
-                'jpg' => 'jpeg',
-                'jpeg' => 'jpeg',
-                'png' => 'png',
-                'gif' => 'gif',
-                'bmp' => 'bmp',
-            ],
-            'sizes' => [
-                'single' => [375, 450],
-                '1024' => [1024, 1024],
-                '175_210' => [175, 210],
-                '350_490' => [350, 490],
-                '360_471' => [360, 471],
-                '100_100' => [100, 100],
-                '200' => [200, 200],
-                '59' => [59, 59],
-                '77' => [77, 77],
-                '350_420' => [350, 420],
-                '360_430' => [360, 430],
-                '129' => [129, 129],
-            ],
-            'text' => [
-                'text' => 'sex-true',
-                'fontFile' => '@app/web/fonts/MullerExtraBold.ttf'
-            ],
-        ],
         'request' => [
             'csrfParam' => '_csrf-frontend',
         ],
