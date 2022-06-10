a97244add15e07bd7739eb92929733c58a61fdb7
Работа <sanyapodolski@gmail.com>
2022-06-10 12:19:56 +0300

    правки с сортировкой

--- frontend/components/helpers/GetOrderHelper.php
+++ frontend/components/helpers/GetOrderHelper.php
@@ -4,7 +4,7 @@ namespace frontend\components\helpers;
 
 class GetOrderHelper
 {
-    private $default = 'tarif_id DESC, sort DESC, fake DESC';
+    private $default = 'tarif_id DESC, fake DESC, sort DESC';
 
     private $priceDesc = 'price DESC';
     private $priceAsc = 'price ASC';
