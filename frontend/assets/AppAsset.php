387a971f54d14233c977548e0f70429d71853aef
Your Name <you@example.com>
2022-06-22 13:46:46 +0300

    Виджет для информации

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,7 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css?v=60',
+        'css/site.css?v=61',
     ];
     public $js = [
         'js/script.js?v=42'
