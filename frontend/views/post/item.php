56d9f53d63a3763a3c85ec29859ecac86d4dccf9
Your Name <you@example.com>
2022-06-28 13:18:01 +0300

    правки по верстке

--- frontend/views/post/item.php
+++ frontend/views/post/item.php
@@ -230,7 +230,7 @@ $countReview = \frontend\modules\user\models\Posts::countReview($post['id']);
                             </div>
                         </div>
 
-                        <div class="post-address">
+                        <div class="post-address post-address-wrap">
                             <div class="geo-icon icon">
                                 <svg width="9" height="9" viewBox="0 0 9 9" fill="none"
                                      xmlns="http://www.w3.org/2000/svg">
