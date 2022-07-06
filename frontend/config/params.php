b919f9b8914c2abca8538aefd8fab3862aef1f1d
Your Name <you@example.com>
2022-07-06 13:55:03 +0300

    доработка формы добавления анкеты

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -3,7 +3,7 @@ return [
     'adminEmail' => 'admin@example.com',
     'bsVersion' => '4.x',
     'advert_limit' => 20,
-    'post_limit' => 12,
+    'post_limit' => 36,
     'site_name' => 'sex-tut.com',
     'min_pay' => 300,
     'view_100_buy_price' => 20,
