12f99046b8593bd611ce6f551f2e0bfd63e6d313
Your Name <you@example.com>
2022-07-14 15:18:09 +0300

    Помошник на всех страница

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,7 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css?v=69',
+        'css/site.css?v=70',
     ];
     public $js = [
         'js/script.js?v=45'
