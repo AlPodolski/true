fd90d41323c9fbe58002a21a662184ff72919d76
Your Name <you@example.com>
2022-07-18 11:40:31 +0300

    равки с картой сайта

--- frontend/views/site/map.php
+++ frontend/views/site/map.php
@@ -10,49 +10,49 @@
 <?php echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'; ?>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?></loc>
-        <lastmod>2022-01-18</lastmod>
+        <lastmod>2022-07-18</lastmod>
         <priority>1</priority>
     </url>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?>/proverennye</loc>
-        <lastmod>2022-01-18</lastmod>
+        <lastmod>2022-07-18</lastmod>
         <priority>0.9</priority>
     </url>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?>/video</loc>
-        <lastmod>2022-01-18</lastmod>
+        <lastmod>2022-07-18</lastmod>
         <priority>0.9</priority>
     </url>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?>/price-do-1500</loc>
-        <lastmod>2022-01-18</lastmod>
+        <lastmod>2022-07-18</lastmod>
         <priority>0.9</priority>
     </url>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?>/price-ot-1500-do-2000</loc>
-        <lastmod>2022-01-18</lastmod>
+        <lastmod>2022-07-18</lastmod>
         <priority>0.9</priority>
     </url>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?>/price-ot-2000-do-3000</loc>
-        <lastmod>2022-01-18</lastmod>
+        <lastmod>2022-07-18</lastmod>
         <priority>0.9</priority>
     </url>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?>/price-ot-6000</loc>
-        <lastmod>2022-01-18</lastmod>
+        <lastmod>2022-07-18</lastmod>
         <priority>0.9</priority>
     </url>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?>/novie</loc>
-        <lastmod>2022-01-18</lastmod>
+        <lastmod>2022-07-18</lastmod>
         <priority>0.9</priority>
     </url>
 
     <?php if ($metro) foreach ($metro as $metroItem) : ?>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?>/metro-<?php echo $metroItem['url']?></loc>
-        <lastmod>2022-01-18</lastmod>
+        <lastmod>2022-07-18</lastmod>
         <priority>0.9</priority>
     </url>
     <?php endforeach; ?>
@@ -60,7 +60,7 @@
     <?php if ($rayon) foreach ($rayon as $rayonItem) : ?>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?>/rayon-<?php echo $rayonItem['url']?></loc>
-        <lastmod>2022-01-18</lastmod>
+        <lastmod>2022-07-18</lastmod>
         <priority>0.9</priority>
     </url>
     <?php endforeach; ?>
@@ -68,7 +68,7 @@
     <?php if ($service) foreach ($service as $serviceItem) : ?>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?>/usluga-<?php echo $serviceItem['url']?></loc>
-        <lastmod>2022-01-18</lastmod>
+        <lastmod>2022-07-18</lastmod>
         <priority>0.9</priority>
     </url>
     <?php endforeach; ?>
@@ -76,7 +76,7 @@
     <?php if ($place) foreach ($place as $placeItem) : ?>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?>/mesto-<?php echo $placeItem['url']?></loc>
-        <lastmod>2022-01-18</lastmod>
+        <lastmod>2022-07-18</lastmod>
         <priority>0.9</priority>
     </url>
     <?php endforeach; ?>
@@ -84,7 +84,7 @@
     <?php if ($naci) foreach ($naci as $naciItem) : ?>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?>/nacionalnost-<?php echo $naciItem['url']?></loc>
-        <lastmod>2022-01-18</lastmod>
+        <lastmod>2022-07-18</lastmod>
         <priority>0.9</priority>
     </url>
     <?php endforeach; ?>
@@ -92,14 +92,14 @@
     <?php if ($hair) foreach ($hair as $hairItem) : ?>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?>/cvet-volos-<?php echo $hairItem['url']?></loc>
-        <lastmod>2022-01-18</lastmod>
+        <lastmod>2022-07-18</lastmod>
         <priority>0.9</priority>
     </url>
     <?php endforeach; ?>
     <?php if ($intimHair) foreach ($intimHair as $intimHairItem) : ?>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?>/intimnaya-strizhka-<?php echo $intimHairItem['url']?></loc>
-        <lastmod>2022-01-18</lastmod>
+        <lastmod>2022-07-18</lastmod>
         <priority>0.9</priority>
     </url>
     <?php endforeach; ?>
@@ -107,7 +107,7 @@
     <?php if ($posts) foreach ($posts as $postItem) : ?>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?>/post/<?php echo $postItem['id']?></loc>
-        <lastmod>2022-01-18 </lastmod>
+        <lastmod>2022-07-18 </lastmod>
         <priority>0.8</priority>
     </url>
     <?php endforeach; ?>
