ea93227384b3fab782f812626d3d2ce7a7e53c79
Your Name <you@example.com>
2022-08-09 15:56:01 +0300

    начало комментариев

--- common/components/service/AddPhoneReview.php
+++ common/components/service/AddPhoneReview.php
@@ -2,12 +2,27 @@
 
 namespace common\components\service;
 
+use common\models\Phone;
+use frontend\modules\user\models\Posts;
+use frontend\modules\user\models\Review;
 use yii\base\Component;
 
 class AddPhoneReview extends Component
 {
     public static function handle($event)
     {
+        $comment = $event->sender;
 
+        /* @var $comment Review */
+
+        $post = Posts::find()->where(['id' => $comment->post_id])->one();
+
+        if ($post and $phone = Phone::find()->where(['phone'])){
+
+
+
+        }
+
+        dd($comment);
     }
 }
