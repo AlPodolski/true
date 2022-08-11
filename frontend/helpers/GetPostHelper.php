24bd045dd99289372705782fa1b5105cd20df5ee
Your Name <you@example.com>
2022-08-11 10:58:26 +0300

    вывод комментариев к номеру

--- frontend/helpers/GetPostHelper.php
+++ frontend/helpers/GetPostHelper.php
@@ -19,7 +19,7 @@ class GetPostHelper
                 ->andWhere(['city_id' => $city_id])
                 ->with('gal', 'metro', 'avatar', 'place', 'service',
                     'sites', 'rayon', 'nacionalnost',
-                    'cvet', 'strizhka', 'selphiCount', 'serviceDesc', 'partnerId','phoneComment'
+                    'cvet', 'strizhka', 'selphiCount', 'serviceDesc', 'partnerId'
                 )
                 ->asArray()->limit(1)->one();
 
