e5e131cbc350ac5ea6217dddbe200aed024475de
Your Name <you@example.com>
2022-08-12 14:03:41 +0300

    правки

--- frontend/helpers/QueryParamsHelper.php
+++ frontend/helpers/QueryParamsHelper.php
@@ -404,6 +404,7 @@ class QueryParamsHelper
 
                 $id = Review::find()->select('post_id')
                     ->where(['is_moderate' => Review::MODARATE])
+                    ->andWhere(['<>', 'text', ''])
                     ->asArray()
                     ->all();
 
