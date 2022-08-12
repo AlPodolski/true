8b75f63cb2122f823af744bd95cf330a93c66115
Your Name <you@example.com>
2022-08-12 14:42:41 +0300

    правки

--- frontend/controllers/FilterController.php
+++ frontend/controllers/FilterController.php
@@ -70,6 +70,7 @@ class FilterController extends Controller
                 ->andWhere(['city_id' => $cityInfo['id']])
                 ->andWhere(['status' => Posts::POST_ON_PUPLICATION_STATUS])
                 ->orderBy(Posts::getOrder())
+                ->cache(3600)
                 ->asArray();
 
             if (\strstr($param, 'novie')) $posts = $posts->orderBy('id DESC');
