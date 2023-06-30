7750197f0e050dd56d8affdfedea46835861b94f
Precision <sanyapodolski@gmail.com>
2023-06-30 10:20:13 +0300

    сделать верстку поиска фильтра

--- frontend/views/find/index.php
+++ frontend/views/find/index.php
@@ -16,54 +16,65 @@ Yii::$app->view->registerMetaTag([
 
 
 ?>
-<div class="container custom-container">
 
-    <h1 class="margin-top-20"> <?php echo $h1 ?> </h1>
-
-    <div class="row">
-
-        <?php echo '<div data-url="/'.$param.'" class="col-12"></div>'; ?>
-
-        <?php if (is_array($posts) and $posts) : ?>
-
-            <?php foreach ($posts as $post) : ?>
-
-                <?php echo $this->renderFile(Yii::getAlias('@app/views/layouts/article.php'), ['post' => $post]); ?>
-
-            <?php endforeach; ?>
-
-        <?php else : ?>
-
-            <div class="col-12">
-                <p class="alert alert-info">по таким параметрам ничего нет  измените  начтройки  фильтра</p>
+<div class="filter__catalog">
+    <div class="container">
+        <div class="row filter__bottom">
+            <div class="filter-sort__left">
+                <h1 class="filter-sort__title"> <?php echo $h1 ?> </h1>
+                <div class="filter-sort__btn" data-filter-btn>
+                    <svg>
+                        <use xlink:href='/svg/dest/stack/sprite.svg#filter'></use>
+                    </svg>
+                    Фильтр
+                </div>
             </div>
 
-        <?php endif; ?>
+            <?php echo \frontend\widgets\SortingWidget::widget() ?>
 
-    </div>
-    <?php if ($posts and count($posts) > 6) : ?>
-        <div class="row content"></div>
-        <svg class="filter" version="1.1">
-            <defs>
-                <filter id="gooeyness">
-                    <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-                    <feColorMatrix in="blur" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 20 -10" result="gooeyness" />
-                    <feComposite in="SourceGraphic" in2="gooeyness" operator="atop" />
-                </filter>
-            </defs>
-        </svg>
-        <div class="dots">
-            <div class="dot mainDot"></div>
-            <div class="dot"></div>
-            <div class="dot"></div>
-            <div class="dot"></div>
-            <div class="dot"></div>
         </div>
+    </div>
+</div>
+
+<div class="catalog">
+    <div class="container">
+
+        <div class="row">
+            <?php echo '<div data-url="/'.$param.'" class="col-12"></div>'; ?>
+        </div>
+
+        <div class="row catalog__items first-content">
+
+            <?php if (is_array($posts) and $posts) : ?>
+
+                <?php foreach ($posts as $post) : ?>
+
+                    <?php echo $this->renderFile(Yii::getAlias('@app/views/layouts/article.php'), ['post' => $post]); ?>
+
+                <?php endforeach; ?>
+
+            <?php else : ?>
+
+                <div class="col-12">
+                    <p class="alert alert-info">по таким параметрам ничего нет  измените  начтройки  фильтра</p>
+                </div>
+
+            <?php endif; ?>
+
+        </div>
+
+        <?php if ($posts and count($posts) > 6) : ?>
+
+        <div class="row content-post catalog__items"></div>
+
         <div class="row">
             <div class="col-12 pager" data-page="1"
                  data-url="<?php echo Yii::$app->request->url ?>"
                  data-adress="<?php echo Yii::$app->request->url ?>"
                  data-reqest="<?php echo Yii::$app->request->url ?>"></div>
         </div>
-    <?php endif; ?>
+
+        <?php endif; ?>
+
+    </div>
 </div>
