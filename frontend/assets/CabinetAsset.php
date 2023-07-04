c9f1726c64291abbca65f65cf557da5f8b77d76e
Precision <sanyapodolski@gmail.com>
2023-07-04 14:40:52 +0300

    edit

--- frontend/assets/CabinetAsset.php
+++ frontend/assets/CabinetAsset.php
@@ -12,7 +12,7 @@ class CabinetAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css?v=113',
+        'css/site.css?v=114',
     ];
     public $js = [
         'js/script.js?v=79',
